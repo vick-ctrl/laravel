6483becaf2ef46d29e1b751edf2a939850dfd629
vick-ctrl <victoria.oliv2015@gmail.com>
2026-02-25 18:22:01 -0300

    incompleto

---
+++ app/Http/Controllers/Aluno.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Aluno extends Controller
+{
+    function rematricula(){
+        echo 'Rematrícula Concluída!';
+    }
+
+    function boletim(){
+        echo '50% MB e 50% B';
+    }
+
+    
+}
+
+
--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,18 @@ use App\Http\Middleware\LogAcessoMiddleware;
 */
 
 Route::get('/', [App\Http\Controllers\Principal::class, 'principal']);
+//aluno
+Route::get('aluno/login', [App\Http\Controllers\Contato::class, 'login']);
+Route::get('aluno/logout', [App\Http\Controllers\Contato::class, 'logout']);
+Route::get('aluno/rematricula', [App\Http\Controllers\Aluno::class, 'rematricula']);
+Route::get('aluno/cancelar', [App\Http\Controllers\Aluno::class, 'cancelarMatricula']);
+Route::get('aluno/boletim', [App\Http\Controllers\Aluno::class, 'boletim']);
+
+//professor
+Route::get('/contato', [App\Http\Controllers\Contato::class, 'contato']);
+Route::get('/contato', [App\Http\Controllers\Contato::class, 'contato']);
+
+
 
 
 
