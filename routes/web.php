0336967478eba7aa587b748c4a1b5a3542b72dca
vick-ctrl <victoria.oliv2015@gmail.com>
2026-03-04 18:19:40 -0300

    aula do 04/03

--- routes/web.php
+++ routes/web.php
@@ -13,18 +13,20 @@ use App\Http\Middleware\LogAcessoMiddleware;
 |
 */
 
-Route::get('/', [App\Http\Controllers\Principal::class, 'principal']);
-//aluno
-Route::get('aluno/login', [App\Http\Controllers\Contato::class, 'login']);
-Route::get('aluno/logout', [App\Http\Controllers\Contato::class, 'logout']);
-Route::get('aluno/rematricula', [App\Http\Controllers\Aluno::class, 'rematricula']);
-Route::get('aluno/cancelar', [App\Http\Controllers\Aluno::class, 'cancelarMatricula']);
-Route::get('aluno/boletim', [App\Http\Controllers\Aluno::class, 'boletim']);
 
-//professor
-Route::get('/contato', [App\Http\Controllers\Contato::class, 'contato']);
-Route::get('/contato', [App\Http\Controllers\Contato::class, 'contato']);
 
+// //aluno
+// Route::get('aluno/login', [App\Http\Controllers\Contato::class, 'login']);
+// Route::get('aluno/logout', [App\Http\Controllers\Contato::class, 'logout']);
+// Route::get('aluno/rematricula', [App\Http\Controllers\Aluno::class, 'rematricula']);
+// Route::get('aluno/cancelar', [App\Http\Controllers\Aluno::class, 'cancelarMatricula']);
+// Route::get('aluno/boletim', [App\Http\Controllers\Aluno::class, 'boletim']);
+
+
+
+
+Route::get('/', [App\Http\Controllers\Principal::class, 'home'])->name('pagina-inicial');
+Route::get('/sobre', [App\Http\Controllers\Sobre::class, 'about'])->name('pagina-sobre');
 
 
 
